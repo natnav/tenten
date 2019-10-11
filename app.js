c0a95224f2f7deea2778db81c58da9bf39f65fbc
Nat <nat.jnava@gmail.com>
2019-10-10 20:35:01 -0700

    rabbit

---
+++ app.js
@@ -0,0 +1,17 @@
+const rabbit = document.getElementById('rabbit')
+
+// rabbit.onclick = () => alert('thanks')
+// rabbit.onclick = () => alert('thanks again')
+
+rabbit.addEventListener('click', () => alert('thanks'))
+rabbit.addEventListener('click', () => alert('thanks again'))
+// rabbit.addEventListener('click', toggleWideClass)
+// rabbit.addEventListener('transitionend', onTransitioned)
+
+// function toggleWideClass() {
+//   rabbit.classList.toggle('wide')
+// }
+
+// function onTransitioned() {
+//   alert('Wide animation triggered')
+// }
